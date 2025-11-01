fd6be832b826a6b91d812cd034bdc3faec802a0e
Fozilbek Fayzullayev <fozilbekfayzullayev05@gmail.com>
2025-11-01 17:38:45 +0500

    feat: update Navbar component to include a list structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,16 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
