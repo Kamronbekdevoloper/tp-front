b9c0544793b398dfe779b01736c5c1767ae7681e
Kamronbek_27 <kamronbekdevoloper@gmail.com>
2025-11-09 00:39:00 +0500

    feat: Add theme provider and mode toggle functionality - Wrapped the RouterProvider with ThemeProvider in App.tsx to manage theme state. - Introduced a Header component with a mode toggle button to switch between light and dark themes. - Created a theme provider context to handle theme state and persist it in local storage. - Added CSS variables for theming and updated styles for dark and light modes. - Implemented a dropdown menu component with various items and functionalities. - Enhanced button component with variant support and improved accessibility.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes";
 
 export default function App() {
   return (
     <div>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -12,3 +11,4 @@ export default defineConfig({
     },
   },
 });
+  
